f59af809d8450ec564f7ac53ab03632ef690eaba
Joseph Ortiz <joeprogrammer88@users.noreply.github.com>
2020-05-27 00:09:44 -0700

    Remove unneeded starting content

---
+++ ChuckNorrisBlazor/Shared/ChuckNorrisResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChuckNorrisBlazor.Shared
+{
+
+    public class ChuckNorrisResponse
+    {
+        public string Type { get; set; }
+
+        public SingleJoke Value { get; set; }
+    }
+
+    public class SingleJoke
+    {
+        public int Id { get; set; }
+
+        public string Joke { get; set; }
+
+        public string[] Categories { get; set; }
+    }
+
+}
